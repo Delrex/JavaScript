f540556f7d7d6f8e599a2982ae7e8efc31b933cb
Delrex <103109588+Delrex@users.noreply.github.com>
2023-06-29 11:50:32 +0300

    Average

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 
-var n = 67;
-getChar(n);
-function getChar (n)
+var arr = [2,4,6,8];
+average(arr);
+function average(arr)
 {
-    var char = String.fromCharCode(n)
-    console.log("Char: " + char);
+    var sum = 0;
+    for(var i = 0; i < arr.length; i++)
+    {
+        sum += arr[i];
+    }
+    var average = sum / arr.length;
+    console.log("Average: " + average);
 }
