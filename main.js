8319e004472e028a3e8a7400065f9498c23e5a52
Delrex <103109588+Delrex@users.noreply.github.com>
2023-06-29 12:00:46 +0300

    Average

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 
-var arr = [2,4,6,8];
+var arr = [2, 4, 6, 8];
 average(arr);
 function average(arr)
 {
-    var sum = 0;
-    for(var i = 0; i < arr.length; i++)
+    if (arr.length == 0)
+    console.error("Array is empty");
+    else
     {
-        sum += arr[i];
-    }
-    var average = sum / arr.length;
-    console.log("Average: " + average);
+        var sum = 0;
+        for(var i = 0; i < arr.length; i++)
+        {
+            sum += arr[i];
+        }
+        var averageNum = sum / arr.length;
+        console.log("Average: " + averageNum);  
+    }  
 }
+
