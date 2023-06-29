5a874e4ddb86c90a75b2ac50f40d21e815f66db5
Delrex <103109588+Delrex@users.noreply.github.com>
2023-06-29 12:51:54 +0300

    Your score is above average?

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-var age = 10;
-if (age < 0)
-console.error("Age can't be negative!!!");
-else if (age >= 2 && age <= 14)
-console.log("Kids drink juice");
-else if (age > 14 && age < 18)
-console.log("Teens drink coke");
-else if (age >= 18 && age < 21)
-console.log("Young adults drink beer");
-else if (age >= 21)
-console.log("Adults drink whisky");
+var yourMark = 8;
+var isMarkBest = false;
+var marks = [5, 6, 9, 6, 7];
+average(marks,yourMark);
+
+function average(marks,yourMark)
+{
+    if (marks.length == 0)
+    console.error("Array is empty");
+    else
+    {
+        var sum = 0;
+        marks.push(yourMark);
+        for(var i = 0; i < marks.length; i++)
+        {
+            sum += marks[i];
+        }
+        var averageMark = sum / marks.length;
+        console.log("Average mark: " + averageMark);
+        if(yourMark > averageMark)
+        return isMarkBest = true;
+        else 
+        return isMarkBest;
+    }  
+}
+console.log("Your score is above average? " + isMarkBest);
